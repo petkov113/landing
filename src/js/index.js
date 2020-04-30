399be5d870cbbdfa1fe8bcd703ca4c07c9d2fbf2
petkov113 <petr.petkov1993@gmail.com>
2020-05-01 01:28:28 +0300

    fix 1

--- src/js/index.js
+++ src/js/index.js
@@ -20,22 +20,25 @@ const sections = [...document.querySelectorAll("section")].map(
 
 let activeLink;
 
-links.forEach(link => {
-  if(link.href === location.href) activeLink = link;
-  else return;
-  activeLink.classList.add('active')
-})
-
-document.addEventListener("scroll", () => {
+const selectLink = () => {
   let currentSection = sections.find(
     section => section.start <= scrollY && section.end > scrollY
   );
+
   if (currentSection) {
     if (activeLink === links[currentSection.index]) return;
-    else {
-      activeLink.classList.remove("active");
+
+    if (!activeLink) {
       activeLink = links[currentSection.index];
+      activeLink.classList.add("active");
+      return;
     }
+
+    activeLink.classList.remove("active");
+    activeLink = links[currentSection.index];
     activeLink.classList.add("active");
   }
-});
+};
+
+document.addEventListener("DOMContentLoaded", selectLink);
+document.addEventListener("scroll", selectLink);
